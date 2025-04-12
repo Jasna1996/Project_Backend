deb8408ea1d7714786fac7965125a0bd8d9d5dbb
Jasna <jasnajaleel1996@gmail.com>
2025-04-12 10:19:35 +0530

    Authorization added by role and re-arranged turf route

---
+++ routes/turfRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const turfRouter = express.Router();
+const upload = require('../middleware/multer');
+const { AddTurf, getAllTurfs, getTurf, editTurf, deleteTurf } = require('../controllers/turfController');
+const authentication = require('../middleware/authentication');
+const authorizedRole = require('../middleware/authorizedRole');
+
+
+turfRouter.post('/createTurf', upload.single("image"), authentication, authorizedRole('admin'), AddTurf);
+turfRouter.get('/getAllTurf', getAllTurfs);
+turfRouter.get('/getTurf/:name', getTurf);
+turfRouter.put('/editTurf/:id', authentication, authorizedRole('admin', 'manager'), editTurf);
+turfRouter.delete('/deleteTurf/:id', authentication, authorizedRole('admin'), deleteTurf)
+
+module.exports = turfRouter;
