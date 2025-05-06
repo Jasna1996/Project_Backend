c816d9f09b7358eef6d94b88ea085f743492b65b
Jasna <jasnajaleel1996@gmail.com>
2025-05-06 20:33:34 +0530

    latest cors update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
     origin: 'https://turfbooking-frontend.vercel.app',
     credentials: true,
     methods: ['get', 'post', 'delete', 'put'],
-    origin: "*",
+    allowedHeaders: ['Access-Control-Allow-Origin','*']
 
 }))
 
