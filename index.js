a0a163cc48ab14bec0734b508d8adf089f9b384d
Jasna <jasnajaleel1996@gmail.com>
2025-05-06 21:03:52 +0530

    cors update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 const express = require('express');
 const { dbConnection } = require('./config/dbConnection');
+const cors = require('cors');
+const serverless = require('serverless-http');
 require("dotenv").config();
 const apiRouter = require('./routes/indexRoute');
-const cors = require('cors');
 const cookieparser = require('cookie-parser');
 
 
@@ -17,10 +18,12 @@ app.use(cookieparser());
 app.use(cors({
     origin: 'https://turfbooking-frontend.vercel.app',
     credentials: true,
-    methods: ['get', 'post', 'delete', 'put'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Access-Control-Allow-Origin','*']
 
 }))
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.json("Server started")
@@ -33,4 +36,5 @@ app.listen(process.env.PORT, () => {
     console.log(`server starts at port ${process.env.PORT}`);
 })
 
-module.exports = { app }
+module.exports =  app ;
+module.exports.handler = serverless(app);
