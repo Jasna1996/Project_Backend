804aef1430a1c18a3fe40010f1ea3567b41cc1c7
Jasna <jasnajaleel1996@gmail.com>
2025-05-08 20:25:08 +0530

    401 issue debug

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -4,8 +4,8 @@ const userModel = require('../models/userModel')
 module.exports = (req, res, next) => {
     try {
 
-        const { token } = req.cookies.token || req.headers.authorization?.split(" ")[1];
         console.log("Headers", req.headers)
+        const { token } = req.cookies.token || req.headers.authorization?.split(" ")[1];
         if (!token) {
             return res.status(401).json({ error: 'jwt not found' })
         }
