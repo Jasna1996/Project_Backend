ffef347005a4bd9041d10049ba057e5806f639ff
Jasna <jasnajaleel1996@gmail.com>
2025-05-08 13:23:49 +0530

    Cors aithorization issue resolved

--- index.js
+++ index.js
@@ -12,20 +12,24 @@ const app = express(); // initialise Express application
 
 dbConnection(); // db connection
 
+app.use(cors({
+    origin: 'https://turfbooking-frontend.vercel.app',
+    credentials: true,
+}));
 
 app.use(express.json());
 app.use(cookieparser());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://turfbooking-frontend.vercel.app');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'https://turfbooking-frontend.vercel.app');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
 
 
 app.use(express.json());
