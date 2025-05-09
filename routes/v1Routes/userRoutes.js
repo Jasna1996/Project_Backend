fb4a38b7f0fde520042bd13629046179a5c57321
Jasna <jasnajaleel1996@gmail.com>
2025-05-09 21:53:40 +0530

    AuthBooking updated

--- routes/v1Routes/userRoutes.js
+++ routes/v1Routes/userRoutes.js
@@ -9,7 +9,7 @@ userRouter.post("/login", login);
 userRouter.get("/profile", authentication, UserProfile);
 userRouter.patch("/update", authentication, updateUser);
 userRouter.delete("/delete/:userId", authentication, deleteUser);
-userRouter.post("/booking", authentication, bookings);
+userRouter.post("/booking", bookings);
 userRouter.get("/getBookings", authentication, getUserBookings);
 userRouter.delete("/cancelBooking/:id", authentication, cancelBooking)
 userRouter.post("/logout", logout);
