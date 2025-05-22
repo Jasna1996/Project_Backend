40b0afb10ed447875d8588f81b28c13cfe8e657d
Jasna <jasnajaleel1996@gmail.com>
2025-05-22 21:03:59 +0530

    auth removed

--- routes/v1Routes/managerRoutes.js
+++ routes/v1Routes/managerRoutes.js
@@ -4,10 +4,10 @@ const { changePassword } = require('../../controllers/userController');
 const managerRouter = require('express').Router();
 const authMiddleware = require('../../middleware/authentication')
 
-managerRouter.get('/turf', authMiddleware, getAllTurfs);
-managerRouter.put('/updateturf/:turfId', authMiddleware, editTurfDetails);
-managerRouter.get('/bookings', authMiddleware, getAllBookings);
-managerRouter.get('/payments', authMiddleware, getManagerPayments);
-managerRouter.post('/changepassword', authMiddleware, changePassword);
+managerRouter.get('/turf', getAllTurfs);
+managerRouter.put('/updateturf/:turfId', editTurfDetails);
+managerRouter.get('/bookings', getAllBookings);
+managerRouter.get('/payments', getManagerPayments);
+managerRouter.post('/changepassword', changePassword);
 
 module.exports = { managerRouter }
